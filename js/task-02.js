a1f0c2b3401bd89525e64348b35a447739239c9e
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-03 01:06:20 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const productsEL = document.createElement("li");
+productsEL.textContent;

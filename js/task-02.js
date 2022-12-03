2f8873d84a71e94eed6107f7388963b5d8af2c9b
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-03 13:38:56 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,5 +7,14 @@ const ingredients = [
   "Condiments",
 ];
 
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsEl = ingredients.map((ingredient) => {
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  element.classList.add("item");
+  return element;
+});
+
 const productsEL = document.createElement("li");
-productsEL.textContent;
+ingredientsList.append(...ingredientsEl);

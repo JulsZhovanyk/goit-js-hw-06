05493196ea5b951173835e74be7f12f7ec960bc3
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-03 14:50:10 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColorEl = document.querySelector(".change-color");
+
+const colorValue = document.querySelector(".color");
+
+buttonChangeColorEl.addEventListener("click", changeColor);
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+}

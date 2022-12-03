fe63365c9c3b088aca387cffc2d33cd2dbda98f3
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-03 14:36:51 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget;
+
+  if (!email.value || !password.value) {
+    alert("Усі поля мають бути заповнені!");
+    return;
+  }
+
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(userData);
+  event.currentTarget.reset();
+}

65db01293b7539e9c128f73e9392acae4a5779f8
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-04 14:49:40 +0200

    Update task-05.js
    add trim()

--- js/task-05.js
+++ js/task-05.js
@@ -5,6 +5,6 @@ inputEl.addEventListener("input", (event) => {
   if (!event.currentTarget.value) {
     spanEl.textContent = "Anonymous";
   } else {
-    spanEl.textContent = event.currentTarget.value;
+    spanEl.textContent = event.currentTarget.value.trim();
   }
 });
